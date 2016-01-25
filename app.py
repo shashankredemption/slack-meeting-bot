64059a6772326ce147315511cd03f5e8f87a6717
Shashank Bhargava <sbhargava@berkeley.edu>
2016-01-25 02:10:09 -0800

    Should actually be using Postgresql if I'm using Heroku

--- app.py
+++ app.py
@@ -1,16 +1,24 @@
 import datetime
 import json
+import logging
 import os
-import redis
-import shelve
+import urlparse
+import sys
 
 from apiclient.discovery import build
 from flask import Flask, request
+from flask.ext.sqlalchemy import SQLAlchemy
 from httplib2 import Http
 from oauth2client.client import SignedJwtAssertionCredentials
 from slacker import Slacker
+from sqlalchemy import create_engine
+from sqlalchemy.orm import scoped_session, sessionmaker
 
 app = Flask(__name__)
+app.logger.addHandler(logging.StreamHandler(sys.stdout))
+app.logger.setLevel(logging.ERROR)
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ["DATABASE_URL"]
+db = SQLAlchemy(app)
 
 client_email = 'calendar-inviter@calendar-inviter.iam.gserviceaccount.com'
 with open("client_secret.json") as f:
@@ -19,14 +27,33 @@ credentials = SignedJwtAssertionCredentials(client_email, private_key,
     'https://www.googleapis.com/auth/calendar')
 http = credentials.authorize(Http())
 service = build('calendar', 'v3', http=http)
-redis_url = os.getenv('REDIS_URL', 'redis://localhost:6379')
-redis = redis.from_url(redis_url)
+
+engine = create_engine(os.environ["DATABASE_URL"])
+connection = engine.connect()
+Session = scoped_session(sessionmaker(autocommit=False, autoflush=True, bind=engine))
+
+class Token(db.Model):
+    __tablename__ = 'token'
+    team_id = db.Column(db.String(20), primary_key=True)
+    access_token = db.Column(db.String(60))
+
+    def __init__(self, team_id, access_token):
+        self.team_id = team_id
+        self.access_token = access_token
 
 @app.route('/auth', methods=['GET'])
 def oauth():
     code = request.args.get('code')
+    session = Session()
     oauth_info = Slacker.oauth.access(os.environ['CLIENT_ID'], os.environ['CLIENT_SECRET'], code).body
-    redis.set(str(oauth_info['team_id']), str(oauth_info['access_token']))
+    t = Token.query.get(str(oauth_info['team_id']))
+    if t:
+        t.access_token = str(oauth_info['access_token'])
+    else:
+        t = Token(str(oauth_info['team_id']), str(oauth_info['access_token']))
+        session.add(t)
+    session.commit()
+    session.close()
     return 'successfully authenticated'
 
 @app.route('/event', methods=['POST'])
@@ -37,7 +64,10 @@ def make_event():
         text = request.form['text']
         if text == "help":
             return "Input is formatted in following way. EVENT NAME from HH:MM PM to HH:MM PM on MM/DD/YY at LOCATION(optional) with @users @to @invite OR @channel to invite everyone"
-        slack = Slacker(redis.get(str(request.form['team_id'])))
+        session = Session()
+        access_token = Token.query.get(str(request.form['team_id'])).access_token
+        session.close()
+        slack = Slacker(str(access_token.strip()))
         channel_id = request.form['channel_id']
         if channel_id.startswith('C'):
             members = slack.channels.info(channel_id).body['channel']['members']
